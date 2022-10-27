3894c51e111bc2b21bf98dad5df7e62027ed2687
dolongdev <dolong.dev@gmail.com>
2022-10-27 13:51:16 +0700

    update template

--- src/main/java/com/qhdn/poly/controller/HomeController.java
+++ src/main/java/com/qhdn/poly/controller/HomeController.java
@@ -3,18 +3,20 @@ package com.qhdn.poly.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServlet;
 
 @Controller
-@RequestMapping("/home")
-public class HomeController {
+public class HomeController extends HttpServlet {
 
-    @GetMapping("")
+    @GetMapping("/home")
     public String home(){
-        return "/page/layout-home";
+        return "/page/home";
     }
 
-    @GetMapping("/admin/accounts")
-    public String admin(){
-        return "/admin/account-list";
+    @GetMapping("/about")
+    public String about(){
+        return "/page/about";
     }
 }
